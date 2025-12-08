b62d46dd3fa60647fdc96bf3c4fb51c0e23572b2
FrankLouwRP <frank.louw@redpathmining.com>
2025-12-08 12:09:19 -0500

    Auto-sync: 2025-12-08 12:09:19

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 (setq use-package-always-ensure t)
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
 (global-set-key [remap list-buffers] 'ibuffer)
-(set-frame-font "Comic Sans MS" nil t)
+(set-frame-font "Iosevka NFM" nil t)
 
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
@@ -33,6 +33,8 @@
 	(sequence "SOMEDAY(s!)")
 	(sequence "CANCELLED(c!)")
         (sequence "DISCUSS(D!)" "|" "RESOLVED(r!)")))
+
+(customize-set-variable 'org-log-into-drawer t)
 
 (setq org-todo-keyword-faces
       '(
